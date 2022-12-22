f022448e6a74b4f544237b8c33e4a91799ff7245
Iain <25081046+Iain-S@users.noreply.github.com>
2022-12-22 23:46:54 +0000

    Add pylint and requirements

--- template.sh
+++ template.sh
@@ -9,7 +9,7 @@ cp "src/template.py" "src/day$DAY.py"
 sed -i "" "s/THE_DAY/$DAY/g" "src/day$DAY.py"
 
 cp "tests/template.py" "tests/test_day$DAY.py"
-sed -i "" "s/THE_DAY/$DAY/g" "tests/day$DAY.py"
+sed -i "" "s/THE_DAY/$DAY/g" "tests/test_day$DAY.py"
 
 touch "examples/day$DAY.txt"
-mkdir -p "inputs/day$DAY.txt"
+touch "inputs/day$DAY.txt"
