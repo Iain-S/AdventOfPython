fe62893de157df8aa77712016f61a8ce03902d85
Iain <25081046+Iain-S@users.noreply.github.com>
2022-12-26 17:21:02 +0000

    Day 14, part 2 (not working)

--- src/animate_day14.py
+++ src/animate_day14.py
@@ -1,16 +1,17 @@
 from asciimatics.screen import Screen
 from time import sleep
 
-from day14 import build_cave
+from day14 import build_cave, get_paths, CaveTwo
 
-# import numpy as np
-# np.set_printoptions(threshold=np.inf, linewidth=600)
+import numpy as np
+np.set_printoptions(threshold=np.inf, linewidth=600)
 
 def animate(screen):
     with open("./examples/day14.txt", encoding="utf-8") as f:
         content = [line.rstrip() for line in f]
 
-    cave = build_cave(content)
+    # cave = build_cave(content)
+    cave = CaveTwo(get_paths(content))
 
     while True:
         screen.print_at(len(cave.static_sand), 0, 0)
@@ -27,7 +28,7 @@ def animate(screen):
             return
 
         screen.refresh()
-        sleep(.1)
+        sleep(.5)
 
 
 Screen.wrapper(animate)
--- src/day14.py
+++ src/day14.py
@@ -1,11 +1,23 @@
 import numpy as np
 
+# def calc_mins_maxs(paths):
+#     xmin = 10_000
+#     xmax = 1
+#     ymax = 0
+#
+#     for path in paths:
+#         for x, y in path:
+#             xmin = min(xmin, x)
+#             xmax = max(xmax, x)
+#             ymax = max(ymax, y)
+#
+#     return xmin, xmax, 0, ymax
+
 
 class Cave:
-    def __init__(self, paths):
+    def calc_mins_maxs(self,paths):
         xmin = 10_000
         xmax = 1
-        ymin = 0
         ymax = 0
 
         for path in paths:
@@ -14,8 +26,21 @@ class Cave:
                 xmax = max(xmax, x)
                 ymax = max(ymax, y)
 
-        self.contents = np.zeros((xmax - xmin + 1, ymax - ymin + 1))
+        return xmin, xmax, 0, ymax
 
+    # def fudge_paths(self, paths):
+    #     # for part 2
+    #     return paths
+
+    def __init__(self, paths):
+        # paths = self.fudge_paths(paths)
+
+        xmin, xmax, ymin, ymax = self.calc_mins_maxs(paths)
+
+        # A 2D copy of the cave's contents, where 0s are air
+        self.contents = np.zeros((ymax - ymin + 1, xmax - xmin + 1))
+
+        # Add the rocks paths as 4s
         for path in paths:
             for start, stop in zip(path[:-1], path[1:]):
                 startx = min(start[0], stop[0]) - xmin
@@ -24,27 +49,31 @@ class Cave:
                 stopy = max(start[1], stop[1]) + 1
                 self.contents[starty:stopy, startx:stopx] = 4
 
+        # Show the source of the sand with a 1
         self.sourcex = 500 - xmin
         self.contents[0, self.sourcex] = 1
-        self.falling_sand = []  # only the sand in motion
-        self.static_sand = []
+
+        # Simulate a stream of sand rather than one grain at a time
+        self.falling_sand = []  # the sand in motion
+        self.static_sand = []  # the sand that has stopped
 
     def take_turn(self):
         i = 0
         while i < len(self.falling_sand):
             # Note these are in y,x not x,y
             particle = self.falling_sand[i]
-        # for i, particle in self.falling_sand:
+
+            # Set the old location of the sand particle back to air
             self.contents[particle[0], particle[1]] = 0
 
-            if self.contents[particle[0]+1, particle[1]] == 0:
+            if self.contents[particle[0] + 1, particle[1]] == 0:
                 # down
                 particle[0] += 1
-            elif self.contents[particle[0]+1, particle[1]-1] == 0:
+            elif self.contents[particle[0] + 1, particle[1] - 1] == 0:
                 # diag left
                 particle[0] += 1
                 particle[1] -= 1
-            elif self.contents[particle[0]+1, particle[1]+1] == 0:
+            elif self.contents[particle[0] + 1, particle[1] + 1] == 0:
                 # diag right
                 particle[0] += 1
                 particle[1] += 1
@@ -53,6 +82,7 @@ class Cave:
                 self.static_sand.append(self.falling_sand.pop(i))
                 i -= 1
 
+            # Set the new location of the sand particle
             self.contents[particle[0], particle[1]] = 3
             i += 1
 
@@ -60,6 +90,29 @@ class Cave:
         assert self.contents[1, self.sourcex] == 0
         self.contents[1, self.sourcex] = 3
         self.falling_sand.append([1, self.sourcex])
+
+class CaveTwo(Cave):
+    def calc_mins_maxs(self,paths):
+        xmin = 10_000
+        xmax = 1
+        ymax = 0
+
+        for path in paths:
+            for x, y in path:
+                xmin = min(xmin, x)
+                xmax = max(xmax, x)
+                ymax = max(ymax, y)
+
+        return xmin, xmax, 0, ymax
+    def __init__(self, paths):
+        xmin, xmax, ymin, ymax = super().calc_mins_maxs(paths)
+
+        # We can, at most, form a triangle as wide as it is high from the start point
+        # paths.append([(0, ymax+2), (1_000, ymax+2)])
+        paths.append([(480, ymax+2), (520, ymax+2)])
+
+        super().__init__(paths)
+
 
 
 def get_paths(lines):
@@ -84,12 +137,18 @@ def one(lines):
         try:
             cave.take_turn()
         except IndexError:
-            # If sand goes outside of the cave then we know it will fall forever
+            # If sand goes outside the cave then we know it will fall forever
             return len(cave.static_sand)
 
 
 def two(lines):
-    pass
+    cave = CaveTwo(get_paths(lines))
+    while True:
+        try:
+            cave.take_turn()
+        except AssertionError:
+            # If sand goes outside the cave then we know it will fall forever
+            return len(cave.static_sand)
 
 
 def main():
