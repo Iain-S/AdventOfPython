56755fdd90d77f2fbf25c4ddc9a701a20d2f1f0f
Iain <25081046+Iain-S@users.noreply.github.com>
2023-01-24 13:51:20 +0000

    Day 19, part 1

---
+++ src/day19.py
@@ -0,0 +1,159 @@
+import re
+import functools
+from multiprocessing import Pool
+
+# counter = 0
+class HashableDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hashed = hash(tuple(x for x in self.values()))
+
+    def __hash__(self):
+        return self.hashed
+
+
+def get_blueprint(line):
+    matches = [int(x) for x in re.findall(r"\d+", line)]
+    assert len(matches) == 7
+    return HashableDict(
+        {
+            "ore_cost": (matches[1],),
+            "clay_cost": (matches[2],),
+            "obsidian_cost": (matches[3], matches[4]),
+            "geode_cost": (matches[5], matches[6]),
+        }
+    )
+
+
+@functools.cache
+def calc_most_geodes(blueprint, minutes, ore, clay, obs, ore_r, clay_r, obs_r):
+    # global counter
+    minutes -= 1
+
+    if minutes == 0:
+        return 0
+
+    new_ore = ore + ore_r
+    new_clay = clay + clay_r
+    new_obs = obs + obs_r
+    geodes = []
+
+    x = False
+    # Build an ore robot
+    (ore_cost,) = blueprint["ore_cost"]
+    if ore_cost <= ore:
+        geodes.append(
+            calc_most_geodes(
+                blueprint,
+                minutes,
+                new_ore - ore_cost,
+                new_clay,
+                new_obs,
+                ore_r + 1,
+                clay_r,
+                obs_r,
+            )
+        )
+    else:
+        if ore_r:
+            x = True
+
+    # Build a clay robot
+    (ore_cost,) = blueprint["clay_cost"]
+    if ore_cost <= ore:
+        geodes.append(
+            calc_most_geodes(
+                blueprint,
+                minutes,
+                new_ore - ore_cost,
+                new_clay,
+                new_obs,
+                ore_r,
+                clay_r + 1,
+                obs_r,
+            )
+        )
+    else:
+        if ore_r:
+            x = True
+
+    # Build an obsidian robot
+    ore_cost, clay_cost = blueprint["obsidian_cost"]
+    if ore_cost <= ore and clay_cost <= clay:
+        geodes.append(
+            calc_most_geodes(
+                blueprint,
+                minutes,
+                new_ore - ore_cost,
+                new_clay - clay_cost,
+                new_obs,
+                ore_r,
+                clay_r,
+                obs_r + 1,
+            )
+        )
+    else:
+        if ore_r and clay_r:
+            x = True
+
+    # Build a geode robot
+    ore_cost, obs_cost = blueprint["geode_cost"]
+    if ore_cost <= ore and obs_cost <= obs:
+        geodes.append(
+            minutes
+            + calc_most_geodes(
+                blueprint,
+                minutes,
+                new_ore - ore_cost,
+                new_clay,
+                new_obs - obs_cost,
+                ore_r,
+                clay_r,
+                obs_r,
+            )
+        )
+    else:
+        if ore_r and obs_r:
+            x = True
+
+    # if len(geodes) < len(blueprint.keys()):
+    if x:
+        # counter += 1
+        # We couldn't afford at least one kind of robot
+        geodes.append(
+            calc_most_geodes(
+                blueprint, minutes, new_ore, new_clay, new_obs, ore_r, clay_r, obs_r
+            )
+        )
+
+    return max(geodes)
+
+
+def run_one(blueprint):
+    return calc_most_geodes(blueprint, 24, 0, 0, 0, 1, 0, 0)
+    # return calc_most_geodes(blueprint, 18, 0, 0, 0, 1, 0, 0)
+
+
+def one(lines):
+    blueprints = [get_blueprint(line) for line in lines]
+    with Pool() as pool:
+        most_geodes = pool.map(run_one, blueprints)
+    # most_geodes = map(run_one, blueprints)
+    quality_levels = [(i + 1) * q for i, q in enumerate(most_geodes)]
+    # print(f"{counter=}")
+    return sum(quality_levels)
+
+
+def two(lines):
+    pass
+
+
+def main():
+    with open("../inputs/day19.txt", encoding="utf-8") as f:
+        lines = [line.rstrip() for line in f]
+    print("one:", one(lines))
+    print("two:", two(lines))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day19.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest.mock import patch
+
+from src.day19 import one, two, get_blueprint, calc_most_geodes, run_one, HashableDict
+
+with open("../examples/day19.txt", encoding="utf-8") as f:
+    content = [line.rstrip() for line in f]
+
+
+class TestOne(unittest.TestCase):
+
+    def test_example(self):
+        expected = 33
+        actual = one(content)
+        self.assertEqual(expected, actual)
+
+    def test_one(self):
+        with patch("src.day19.calc_most_geodes") as mock_geodes:
+            mock_geodes.side_effect = [9, 12]
+
+            expected = 33
+            actual = one(content)
+            self.assertEqual(expected, actual)
+
+    def test_run_one(self):
+
+        expected = 9
+        actual = run_one(get_blueprint(content[0]))
+        self.assertEqual(expected, actual)
+
+        expected = 12
+        actual = run_one(get_blueprint(content[1]))
+        self.assertEqual(expected, actual)
+
+    def test_blueprints(self):
+        expected = HashableDict({
+            "ore_cost": (4,),
+            "clay_cost": (2, ),
+            "obsidian_cost": (3, 14),
+            "geode_cost": (2, 7),
+        })
+        actual = get_blueprint("Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.")
+        self.assertEqual(expected, actual)
+
+
+class TestTwo(unittest.TestCase):
+    def test_example(self):
+
+        expected = -1
+        actual = two(content)
+        self.assertEqual(expected, actual)
+
+
+if __name__ == "__main__":
+    unittest.main()
