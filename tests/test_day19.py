ddf1b02beac3a652b5275c29ddd1b20fe2d2f5da
Iain <25081046+Iain-S@users.noreply.github.com>
2023-01-25 15:29:27 +0000

    Day 19, part 2 WIP

--- tests/test_day19.py
+++ tests/test_day19.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from src.day19 import one, two, get_blueprint, calc_most_geodes, run_one, HashableDict
+from src.day19 import one, two, get_blueprint, run_one, HashableDict
 
 with open("../examples/day19.txt", encoding="utf-8") as f:
     content = [line.rstrip() for line in f]
@@ -23,7 +23,6 @@ class TestOne(unittest.TestCase):
             self.assertEqual(expected, actual)
 
     def test_run_one(self):
-
         expected = 9
         actual = run_one(get_blueprint(content[0]))
         self.assertEqual(expected, actual)
@@ -35,19 +34,29 @@ class TestOne(unittest.TestCase):
     def test_blueprints(self):
         expected = HashableDict({
             "ore_cost": (4,),
-            "clay_cost": (2, ),
+            "clay_cost": (2,),
             "obsidian_cost": (3, 14),
             "geode_cost": (2, 7),
         })
-        actual = get_blueprint("Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.")
+        actual = get_blueprint(
+            "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.")
         self.assertEqual(expected, actual)
 
 
 class TestTwo(unittest.TestCase):
     def test_example(self):
-
         expected = -1
         actual = two(content)
+        self.assertEqual(expected, actual)
+
+    def test_run_two(self):
+        expected = 56 * 62
+        actual = two(content[0:2])
+        self.assertEqual(expected, actual)
+
+    def test_run_one(self):
+        expected = 62
+        actual = run_one(get_blueprint(content[1]), 32)
         self.assertEqual(expected, actual)
 
 
