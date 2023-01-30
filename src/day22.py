64ea5c5a2b79467985d3e4a93b484e878ab168fe
Iain <25081046+Iain-S@users.noreply.github.com>
2023-01-30 19:54:16 +0000

    Day 22, Part 2, WIP

--- src/day22.py
+++ src/day22.py
@@ -1,5 +1,6 @@
 from itertools import chain
 
+testing = False
 
 def split_on(a_string, char):
     b = a_string.split(char)
@@ -72,11 +73,11 @@ def simulate(board_map, instructions):
             direction = ROTATIONS[direction + instruction]
             board_map[pos[0]][pos[1]] = ARROWS[direction]
         else:
-            if direction in ("R", "L"):
-                row = board_map[pos[0]]
+            for _ in range(instruction):
+                if direction in ("R", "L"):
+                    row = board_map[pos[0]]
 
-                if direction == "R":
-                    for _ in range(instruction):
+                    if direction == "R":
                         if pos[1] + 1 >= len(row):
                             # wrap around
                             x = min([row.index(z) for z in NOT_EMTPY if z in row])
@@ -92,9 +93,8 @@ def simulate(board_map, instructions):
                             # move right
                             pos = next_pos
                             board_map[pos[0]][pos[1]] = ARROWS[direction]
-                else:
-                    # Left
-                    for _ in range(instruction):
+                    else:
+                        # Left
                         if pos[1] - 1 < 0 or row[pos[1] - 1] == " ":
                             # wrap around
                             x = len(row) - 1
@@ -110,36 +110,34 @@ def simulate(board_map, instructions):
                             # move left
                             pos = next_pos
                             board_map[pos[0]][pos[1]] = ARROWS[direction]
-            else:
-                # Up or Down
-                above = pos[0]
-                while True:
-                    if above - 1 < 0 or board_map[above - 1][pos[1]] == " ":
-                        break
-                    else:
-                        above -= 1
+                else:
+                    # Up or Down
+                    above = pos[0]
+                    while True:
+                        if above - 1 < 0 or board_map[above - 1][pos[1]] == " ":
+                            break
+                        else:
+                            above -= 1
 
-                below = pos[0]
-                while True:
-                    if (
-                        below + 1 >= len(board_map)
-                        or pos[1] >= len(board_map[below + 1])
-                        or board_map[below + 1][pos[1]] == " "
-                    ):
-                        break
-                    else:
-                        below += 1
+                    below = pos[0]
+                    while True:
+                        if (
+                            below + 1 >= len(board_map)
+                            or pos[1] >= len(board_map[below + 1])
+                            or board_map[below + 1][pos[1]] == " "
+                        ):
+                            break
+                        else:
+                            below += 1
 
-                # column = [board_map[i][pos[1]] for i in range(above, below+1)]
-                column = [c[pos[1]] for c in board_map[above : below + 1]]
+                    column = [c[pos[1]] for c in board_map[above : below + 1]]
 
-                if column[0] == " " or column[-1] == " ":
-                    print("error!")
+                    if column[0] == " " or column[-1] == " ":
+                        print("error!")
 
-                r_pos = pos[0] - above, pos[1]
+                    r_pos = pos[0] - above, pos[1]
 
-                if direction == "D":
-                    for _ in range(instruction):
+                    if direction == "D":
                         if r_pos[0] + 1 >= len(column):
                             # wrap around
                             y = 0
@@ -159,9 +157,8 @@ def simulate(board_map, instructions):
                             r_pos = next_r_pos
                             board_map[r_pos[0] + above][r_pos[1]] = ARROWS[direction]
 
-                else:
-                    # Up
-                    for _ in range(instruction):
+                    else:
+                        # Up
                         if r_pos[0] - 1 < 0:
                             # wrap around
 
@@ -180,13 +177,281 @@ def simulate(board_map, instructions):
                             r_pos = next_r_pos
                             board_map[r_pos[0] + above][r_pos[1]] = ARROWS[direction]
 
-                pos = r_pos[0] + above, r_pos[1]
+                    pos = r_pos[0] + above, r_pos[1]
 
     return pos, direction
 
 
+def new_direction_pos(direction, pos):
+    # change direction and y,x position
+    if testing:
+        if direction == "R":
+            if 0 <= pos[0] <= 3:
+                direction = "L"
+                pos = 8 + 3 - pos[0], 15
+
+            elif 4 <= pos[0] <= 7:
+                direction = "D"
+                # pos = 8, pos[0]-7+12
+                # pos = 8, 16 - (8 - pos[0])
+                pos = 8, 15 - (pos[0]-4)
+
+            elif 8 <= pos[0] <= 11:
+                direction = "L"
+                pos = 3 - (pos[0] - 8), 11
+
+            else:
+                assert False, pos
+
+        elif direction == "L":
+            if 0 <= pos[0] <= 3:
+                direction = "D"
+                pos = 4, pos[0] + 4
+
+            elif 4 <= pos[0] <= 7:
+                direction = "U"
+                pos = 11, 12 + ( 8 - pos[0])
+
+            elif 8 <= pos[0] <= 11:
+                direction = "U"
+                pos = 7, 7 - (pos[0]-8)
+
+            else:
+                assert False, pos
+
+        elif direction == "U":
+            if 0 <= pos[1] <= 3:
+                direction = "D"
+                pos = 0, 11-pos[1]
+            elif 4 <= pos[1] <= 7:
+                direction = "R"
+                pos = pos[1]-4, 8
+            elif 8 <= pos[1] <= 11:
+                direction = "D"
+                pos = 4, 8 - (pos[1] - 8)
+            elif 12 <= pos[1] <= 15:
+                direction = "L"
+                pos = 11 - (pos[1] - 12), 11
+            else:
+                assert False, pos
+
+        else:
+            # Down
+            if 0 <= pos[1] <= 3:
+                direction = "U"
+                pos = 11, 11-pos[1]
+            elif 4 <= pos[1] <= 7:
+                direction = "R"
+                pos = 7, 11 - (pos[1]-4)
+            elif 8 <= pos[1] <= 11:
+                direction = "U"
+                pos = 7, 3-(pos[1]-8)
+            elif 12 <= pos[1] <= 15:
+                direction = "R"
+                pos = 4 + (pos[1]-12), 0
+            else:
+                assert False, pos
+
+    else:
+        # not testing
+        dim = 5
+        if direction == "R":
+            if 0 <= pos[0] <= dim-1:
+                direction = "L"
+                pos = (3*dim) - 1 - pos[0], (dim*2)-1
+            elif dim <= pos[0] <= (2*dim)-1:
+                direction = "U"
+                pos = dim-1, (2*dim) + (pos[0]-dim)
+            elif 2*dim <= pos[0] <= (3*dim)-1:
+                direction = "L"
+                pos = dim-(pos[0]-(2*dim))-1, (3*dim)-1
+            else:
+                # todo
+                direction = ""
+                pass
+
+
+        elif direction == "L":
+            pass
+        elif direction == "D":
+            pass
+        else:
+            # Up
+            pass
+
+    return direction, pos
+
+
+def simulate_two(board_map, instructions):
+    pos = 0, board_map[0].index(".")  # y, x from top left
+    direction = "R"
+    board_map[pos[0]][pos[1]] = ARROWS[direction]
+
+    for i_inst, instruction in enumerate(instructions):
+        if isinstance(instruction, str):
+            direction = ROTATIONS[direction + instruction]
+            board_map[pos[0]][pos[1]] = ARROWS[direction]
+        else:
+            for _ in range(instruction):
+                if direction in ("R", "L"):
+                    row = board_map[pos[0]]
+
+                    if direction == "R":
+                        if pos[1] + 1 >= len(row):
+                            # wrap around
+                            next_direction, next_pos = new_direction_pos(direction, pos)
+                            if board_map[next_pos[0]][next_pos[1]] == "#":
+                                # stop
+                                break
+                            else:
+                                pos = next_pos
+                                direction = next_direction
+                                board_map[pos[0]][pos[1]] = ARROWS[direction]
+                        else:
+                            x = pos[1] + 1
+                            next_pos = pos[0], x
+                            tile_to_right = row[x]
+
+                            if tile_to_right == "#":
+                                # stop
+                                break
+                            else:
+                                # move right
+                                pos = next_pos
+                                board_map[pos[0]][pos[1]] = ARROWS[direction]
+                    else:
+                        # Left
+                        if pos[1] - 1 < 0 or row[pos[1] - 1] == " ":
+                            # wrap around
+                            next_direction, next_pos = new_direction_pos(direction, pos)
+                            if board_map[next_pos[0]][next_pos[1]] == "#":
+                                # stop
+                                break
+                            else:
+                                pos = next_pos
+                                direction = next_direction
+                                board_map[pos[0]][pos[1]] = ARROWS[direction]
+                        else:
+                            x = pos[1] - 1
+
+                            next_pos = pos[0], x
+                            tile_to_left = row[x]
+                            if tile_to_left == "#":
+                                # stop
+                                break
+                            else:
+                                # move left
+                                pos = next_pos
+                                board_map[pos[0]][pos[1]] = ARROWS[direction]
+                else:
+                    # Up or Down
+                    above = pos[0]
+                    while True:
+                        if above - 1 < 0 or len(board_map[above-1]) < pos[1] or board_map[above - 1][pos[1]] == " ":
+                            break
+                        else:
+                            above -= 1
+
+                    below = pos[0]
+                    while True:
+                        if (
+                                below + 1 >= len(board_map)
+                                or pos[1] >= len(board_map[below + 1])
+                                or board_map[below + 1][pos[1]] == " "
+                        ):
+                            break
+                        else:
+                            below += 1
+
+                    column = [c[pos[1]] for c in board_map[above : below + 1]]
+
+                    if column[0] == " " or column[-1] == " ":
+                        print("error!")
+
+                    r_pos = pos[0] - above, pos[1]
+
+                    if direction == "D":
+                        if r_pos[0] + 1 >= len(column):
+                            # wrap around
+                            next_direction, next_pos = new_direction_pos(direction, pos)
+                            if board_map[next_pos[0]][next_pos[1]] == "#":
+                                # stop
+                                break
+                            else:
+                                pos = next_pos
+                                direction = next_direction
+                                board_map[pos[0]][pos[1]] = ARROWS[direction]
+                        else:
+                            y = r_pos[0] + 1
+
+                            next_r_pos = y, r_pos[1]
+                            tile_below = column[y]
+                            if tile_below == "#":
+                                # stop
+                                break
+                            else:
+                                # move down
+                                r_pos = next_r_pos
+                                board_map[r_pos[0] + above][r_pos[1]] = ARROWS[direction]
+
+                            pos = r_pos[0] + above, r_pos[1]
+
+                    else:
+                        # Up
+                        if r_pos[0] - 1 < 0:
+                            # wrap around
+                            next_direction, next_pos = new_direction_pos(direction, pos)
+                            if board_map[next_pos[0]][next_pos[1]] == "#":
+                                # stop
+                                break
+                            else:
+                                pos = next_pos
+                                direction = next_direction
+                                board_map[pos[0]][pos[1]] = ARROWS[direction]
+                        else:
+                            y = r_pos[0] - 1
+
+                            next_r_pos = y, r_pos[1]
+                            tile_above = column[y]
+                            if tile_above == "#":
+                                # stop
+                                break
+                            else:
+                                # move up
+                                r_pos = next_r_pos
+                                board_map[r_pos[0] + above][r_pos[1]] = ARROWS[direction]
+
+                            pos = r_pos[0] + above, r_pos[1]
+
+    return pos, direction
+
+
+class Cube:
+    def __init__(self,
+                 front_face,
+                 left_face,
+                 right_face,
+                 back_face,
+                 top_face,
+                 bottom_face):
+        pass
+
+
+
+
 def two(content):
-    pass
+    a, b = content.split("\n\n")
+    board_map = [list(line) for line in a.split("\n")]
+    instructions = parse_instructions(b)
+
+    last_position, last_facing = simulate_two(board_map, instructions)
+
+    [print("".join(x)) for x in board_map]
+
+    rownum = last_position[0] + 1
+    colnum = last_position[1] + 1
+    facing_val = FACING_VALUES[last_facing]
+    return (1000 * rownum) + (4 * colnum) + facing_val
 
 
 def main():
--- tests/test_day22.py
+++ tests/test_day22.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from src.day22 import one, two, parse_instructions
+from src.day22 import one, two, parse_instructions, simulate_two, new_direction_pos
 
 with open("../examples/day22.txt", encoding="utf-8") as f:
     content = f.read()
@@ -9,7 +9,6 @@ with open("../examples/day22.txt", encoding="utf-8") as f:
 
 class TestOne(unittest.TestCase):
     def test_example(self):
-
         expected = 6032
         actual = one(content)
         self.assertEqual(expected, actual)
@@ -26,19 +25,37 @@ class TestOne(unittest.TestCase):
             expected = (1000 * 2) + (4 * 3) + 3
             self.assertEqual(expected, actual)
 
-    # def test_make_col(self):
-    #     expected = []
-    #     actual = make_col()
-    #     self.assertListEqual(expected, actual)
-
 
 class TestTwo(unittest.TestCase):
     def test_example(self):
 
-        expected = -1
+        expected = 5031
         actual = two(content)
         self.assertEqual(expected, actual)
 
+    # def test_new_direction_pos(self):
+    #     new_direction_pos()
+
+    def test_simulate_two(self):
+        with open("../examples/day22_II.txt", encoding="utf-8") as f:
+            lines = f.readlines()
+
+
+        # board_map = [list(line)[:-1] for line in lines]
+        # instructions = [10]
+        # simulate_two(board_map, instructions)
+
+        # board_map = [list(line)[:-1] for line in lines]
+        # instructions = ["R", 5, "L", 5]
+        # simulate_two(board_map, instructions)
+        #
+        # board_map = [list(line)[:-1] for line in lines]
+        # instructions = ["R", 5, "L", 5]
+        # simulate_two(board_map, instructions)
+
+        board_map = [list(line)[:-1] for line in lines]
+        instructions = ["R", 14, "L", 6]
+        simulate_two(board_map, instructions)
 
 if __name__ == "__main__":
     unittest.main()
