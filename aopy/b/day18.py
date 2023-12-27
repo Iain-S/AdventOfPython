51d62fb2b225279edebdd8ea6b1a05f83b992de3
Iain-S <25081046+Iain-S@users.noreply.github.com>
2023-12-27 14:40:55 +0000

    WIP Day 18, part 2

--- aopy/b/day18.py
+++ aopy/b/day18.py
@@ -1,6 +1,6 @@
 """Solve the day's problem."""
-import math
-from operator import add
+from operator import add, mul
+from functools import partial
 
 compass = {
     "U": (0, -1),
@@ -35,9 +35,9 @@ def dig_loop(lines: list[str]) -> list[str]:
     return ["".join(x) for x in loop]
 
 
-def dig_interior(loop: list[str]):
+def dig_interior(loop_: list[str]) -> list[str]:
     """Dig the interior."""
-    loop = [list(x) for x in loop]
+    loop = [list(x) for x in loop_]
     changed = 1
 
     # hope that the middle is inside the loop
@@ -50,10 +50,10 @@ def dig_interior(loop: list[str]):
             for x in range(1, len(loop[0]) - 1):
                 if loop[y][x] == ".":
                     if (
-                        loop[y - 1][x] == "*"
-                        or loop[y + 1][x] == "*"
-                        or loop[y][x - 1] == "*"
-                        or loop[y][x + 1] == "*"
+                            loop[y - 1][x] == "*"
+                            or loop[y + 1][x] == "*"
+                            or loop[y][x - 1] == "*"
+                            or loop[y][x + 1] == "*"
                     ):
                         loop[y][x] = "*"
                         changed += 1
@@ -73,19 +73,30 @@ def write_to_file(lines: list[str], filename: str) -> None:
 
 
 def one(lines: list[str]) -> int:
-    loop = dig_loop(lines)
-    write_to_file(loop, "./day18_loop.txt")
+    # loop = dig_loop(lines)
+    # write_to_file(loop, "./day18_loop.txt")
+    #
+    # excavation = dig_interior(loop)
+    # write_to_file(excavation, "./day18_exc.txt")
+    #
+    # plan_a = sum([x.count("#") for x in excavation])
+    plan_a = 52_035
+    print("Plan A: ", plan_a)
 
-    excavation = dig_interior(loop)
-    write_to_file(excavation, "./day18_exc.txt")
-
-    return sum([x.count("#") for x in excavation])
+    lines_b = [line.split(" ") for line in lines]
+    lines_c = [(line[0], int(line[1])) for line in lines_b]
+    loop = walk_loop(lines_c)
+    bounding_rect = calc_bounding_rect(loop)
+    # sys.setrecursionlimit(1500)
+    plan_b = get_area(loop, bounding_rect)
+    print("Plan B: ", plan_b)
+    return plan_b
 
 
 def two(lines: list[str]) -> int:
     new_lines = []
     for line in lines:
-        hexa = line[line.index("#") + 1 : -1]
+        hexa = line[line.index("#") + 1: -1]
         direction = {
             "0": "R",
             "1": "D",
@@ -95,15 +106,7 @@ def two(lines: list[str]) -> int:
         new_lines.append(" ".join([direction, str(int(hexa[:-1], 16)), ""]))
 
     distances = set(int(x.split(" ")[1]) for x in new_lines)
-    gcd = math.gcd(*distances)
-
-    loop = dig_loop(new_lines)
-    # write_to_file(loop, "./day18_loop.txt")
-
-    excavation = dig_interior(loop)
-    # write_to_file(excavation, "./day18_exc.txt")
-
-    return sum([x.count("#") for x in excavation])
+    return 0
 
 
 def main() -> None:
@@ -114,6 +117,144 @@ def main() -> None:
     with open("./inputs/b/day18.txt", encoding="utf-8") as f:
         lines = [line.rstrip() for line in f]
     print("two:", two(lines))
+
+
+tpoint = tuple[int, int]
+tpointpair = tuple[tpoint, tpoint]
+trect = tpointpair
+tloop = list[tpointpair]
+
+
+def walk_loop(directions: list[tuple[str, int]]) -> tloop:
+    """Follow the instructions and take notes of our position."""
+    x, y = 0, 0
+    result = []
+
+    for direction in directions:
+        newx, newy = map(
+            add, (x, y), map(partial(mul, direction[1]), compass[direction[0]])
+        )
+        result.append(((x, y), (newx, newy)))
+        x, y = newx, newy
+
+    return result
+
+
+def calc_bounding_rect(loop: tloop) -> trect:
+    """Get a rectangle that bounds the loop."""
+    top_left = (min([x[0][0] for x in loop]) - 1, min([x[0][1] for x in loop]) - 1)
+    bottom_right = (max([x[1][0] for x in loop]) + 1, max([x[1][1] for x in loop]) + 1)
+    return top_left, bottom_right
+
+
+def rect_in_loop(loop: tloop, rect: trect) -> bool:
+    """Is rect entirely within loop?"""
+    # We can assume that the loop and rectangle don't intersect.
+    bounding_rect = calc_bounding_rect(loop)
+    a_line = rect[0], (rect[0][0], bounding_rect[0][1])
+
+    # If the new line intersects the loop, the rectangle is entirely within the loop.
+    return intersects(loop, a_line)
+
+
+def point_in_rect(point: tpoint, rect: trect) -> bool:
+    """Is the point inside the rectangle?"""
+    top_left, bottom_right = rect
+    if (point[0] >= top_left[0] and point[1] >= top_left[1]) and (
+            point[0] <= bottom_right[0] and point[1] <= bottom_right[1]
+    ):
+        return True
+    return False
+
+
+def intersects(loop: tloop, rect: trect) -> bool:
+    """Does the loop cut across the rectangle?"""
+    top_left, bottom_right = rect
+    for starts_at, finishes_at in loop:
+        # If either end of the line segment is in the rectangle
+        if point_in_rect(starts_at, rect) or point_in_rect(finishes_at, rect):
+            return True
+
+        # Else, if it cuts through vertically...
+        if top_left[0] <= starts_at[0] <= bottom_right[0] and (
+                (starts_at[1] < top_left[1] and finishes_at[1] > bottom_right[1])
+                or (starts_at[1] < bottom_right[1] and finishes_at[1] < top_left[1])
+        ):
+            return True
+
+        # Else, if it cuts through horizontally...
+        x = top_left[1] <= starts_at[1] <= bottom_right[1]
+        y = starts_at[0] < top_left[0] and finishes_at[0] > bottom_right[0]
+        z = starts_at[0] < bottom_right[0] and finishes_at[0] < top_left[0]
+        if x and (y or z):
+            return True
+
+    return False
+
+
+def split_range(start: int, stop: int) -> tuple[tuple[int, int], tuple[int, int]]:
+    diff = stop - start
+    return (start, start - 1 + ((diff + 1) // 2)), (start + ((diff + 1) // 2), stop)
+
+
+def split_rect(rect: trect) -> tuple[trect, ...]:
+    """Split rect into quadrants."""
+
+    size = (rect[1][0] - rect[0][0] + 1) * (rect[1][1] - rect[0][1] + 1)
+    assert size > 1
+
+    left, right = split_range(rect[0][0], rect[1][0])
+    upper, lower = split_range(rect[0][1], rect[1][1])
+
+    # top left
+    tl = ((left[0], upper[0]), (left[1], upper[1]))
+    # top right
+    tr = ((right[0], upper[0]), (right[1], upper[1]))
+    # bottom right
+    br = ((right[0], lower[0]), (right[1], lower[1]))
+    # bottom left
+    bl = ((left[0], lower[0]), (left[1], lower[1]))
+
+    if size == 2:
+        if rect[0][0] == rect[1][0]:
+            # vertical
+            assert tr != br
+            return tr, br
+        else:
+            # horizontal
+            assert bl != br
+            return bl, br
+
+    assert tl[0][0] <= tl[1][0]
+    assert tl[0][1] <= tl[1][1]
+    assert tr[0][0] <= tr[1][0]
+    assert tr[0][1] <= tr[1][1]
+    assert br[0][0] <= br[1][0]
+    assert br[0][1] <= br[1][1]
+    assert bl[0][0] <= bl[1][0]
+    assert bl[0][1] <= bl[1][1]
+
+    return tl, tr, br, bl
+
+
+def get_area(loop: tloop, rect: trect) -> int:
+    """Get the area of the rect that is enclosed by loop."""
+    rect_area = (rect[1][0] - rect[0][0] + 1) * (rect[1][1] - rect[0][1] + 1)
+    if intersects(loop, rect):
+        if rect_area == 1:
+            # Count the loop in the area calculation.
+            return 1
+
+        # Sum the areas of the four quadrants.
+        rects = split_rect(rect)
+        areas = [get_area(loop, x) for x in rects]
+        return sum(areas)
+        # return sum([get_area(loop, x) for x in split_rect(rect)])
+    elif rect_in_loop(loop, rect):
+        return rect_area
+
+    # The rectangle must be entirely outside the loop.
+    return 0
 
 
 if __name__ == "__main__":
--- tests/b/test_day18.py
+++ tests/b/test_day18.py
@@ -1,6 +1,18 @@
 """Tests for the day's solutions."""
 import unittest
-from aopy.b.day18 import one, two, dig_interior, dig_loop
+from aopy.b.day18 import (
+    one,
+    two,
+    dig_interior,
+    dig_loop,
+    walk_loop,
+    get_area,
+    intersects,
+    point_in_rect,
+    split_rect,
+    split_range,
+    calc_bounding_rect,
+)
 
 
 class TestOne(unittest.TestCase):
@@ -51,10 +63,142 @@ class TestTwo(unittest.TestCase):
         with open("../../examples/b/day18.txt", encoding="utf-8") as f:
             self.content = [line.rstrip() for line in f]
 
+        self.two_loop = [
+            ((0, 0), (2, 0)),
+            ((2, 0), (2, 2)),
+            ((2, 2), (0, 2)),
+            ((0, 2), (0, 0)),
+        ]
+
     def test_example(self) -> None:
         expected = 952_408_144_115
         actual = two(self.content)
         self.assertEqual(expected, actual)
+
+    def test_walk_loop(self) -> None:
+        actual = walk_loop(
+            [
+                ("R", 2),
+                ("D", 2),
+                ("L", 2),
+                ("U", 2),
+            ]
+        )
+        expected = self.two_loop
+        self.assertListEqual(expected, actual)
+
+        actual = walk_loop(
+            [
+                ("U", 50),
+                ("U", 50),
+                ("L", 100),
+                ("D", 100),
+                ("R", 100),
+            ]
+        )
+        expected = [
+            ((0, 0), (0, -50)),
+            ((0, -50), (0, -100)),
+            ((0, -100), (-100, -100)),
+            ((-100, -100), (-100, 0)),
+            ((-100, 0), (0, 0)),
+        ]
+        self.assertListEqual(expected, actual)
+
+    def test_get_area(self):
+        expected = 0
+        actual = get_area([], tuple())
+        self.assertEqual(expected, actual)
+
+        loop = self.two_loop
+        rect = ((0, 0), (1, 1))
+        expected = 1
+        actual = get_area(loop, rect)
+        self.assertEqual(expected, actual)
+
+    def test_intersects(self):
+        # rect is inside the loop
+        actual = intersects(self.two_loop, ((1, 1), (1, 1)))
+        self.assertFalse(actual)
+
+        # rect and loop overlap
+        actual = intersects(self.two_loop, ((1, 1), (3, 3)))
+        self.assertTrue(actual)
+
+        # section of loop passes through horizontally
+        actual = intersects([((0, 2), (8, 2))], ((1, 1), (3, 3)))
+        self.assertTrue(actual)
+
+        # section of loop passes through vertically
+        actual = intersects([((2, -10), (2, 5))], ((1, 1), (3, 3)))
+        self.assertTrue(actual)
+
+    def test_point_in_rect(self):
+        self.assertTrue(point_in_rect((0, 0), ((0, 0), (1, 1))))
+
+        self.assertFalse(point_in_rect((-1, -1), ((0, 0), (1, 1))))
+
+    def test_split_rect(self):
+        expected = (
+            ((0, 0), (0, 0)),
+            ((1, 0), (1, 0)),
+            ((1, 1), (1, 1)),
+            ((0, 1), (0, 1)),
+        )
+        actual = split_rect(((0, 0), (1, 1)))
+        self.assertTupleEqual(expected, actual)
+
+        expected = (
+            ((2, 7), (2, 7)),
+            ((3, 7), (4, 7)),
+            ((3, 8), (4, 9)),
+            ((2, 8), (2, 9)),
+        )
+
+        actual = split_rect(((2, 7), (4, 9)))
+        self.assertTupleEqual(expected, actual)
+
+        expected = (
+            ((1, 8), (1, 8)),
+            ((2, 8), (2, 8)),
+        )
+
+        actual = split_rect(((1, 8), (2, 8)))
+        self.assertTupleEqual(expected, actual)
+
+        expected = (
+            ((-27, -126), (-27, -126)),
+            ((-27, -125), (-27, -125)),
+        )
+
+        actual = split_rect(((-27, -126), (-27, -125)))
+        self.assertTupleEqual(expected, actual)
+
+        return
+        # todo unit rectangle
+        with self.assertRaises(RuntimeError):
+            split_rect(((0, 0), (0, 0)))
+
+    def test_split_range(self) -> None:
+        expected = ((0, 1), (2, 3))
+        actual = split_range(0, 3)
+        self.assertTupleEqual(expected, actual)
+
+        expected = ((2, 3), (4, 6))
+        actual = split_range(2, 6)
+        self.assertTupleEqual(expected, actual)
+
+        expected = ((-3, -2), (-1, 0))
+        actual = split_range(-3, 0)
+        self.assertTupleEqual(expected, actual)
+
+    def test_rect_in_loop(self) -> None:
+        pass
+
+    def test_bounding_rect(self) -> None:
+        expected = ((-1, -1), (3, 3))
+        actual = calc_bounding_rect(self.two_loop)
+        self.assertTupleEqual(expected, actual)
 
 
 if __name__ == "__main__":
